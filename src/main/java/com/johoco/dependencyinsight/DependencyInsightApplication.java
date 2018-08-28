1fbdafeb85bbc539fad5f4de66b8193a8d614c1d
John Hoestje <jhoestje@keyholesoftware.com>
2018-08-27 21:03:59 -0500

    Initial commit

---
+++ src/main/java/com/johoco/dependencyinsight/DependencyInsightApplication.java
@@ -0,0 +1,12 @@
+package com.johoco.dependencyinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInsightApplication.class, args);
+	}
+}
